9e7361d9d7add904b9163fd545626f3a443df198
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2025-05-03 22:12:42 +0530

    worked on courses

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -47,7 +47,7 @@ function Courses() {
                 <CardItem
                   translateZ={20}
                   as="a"
-                  href="https://twitter.com/mannupaaji"
+                  // href="https://twitter.com/mannupaaji"
                   target="__blank"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
